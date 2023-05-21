1a4a55ee4c8c8727a83465a0ea6fa3c9a411954b
Tarek-Saci <tarek7saci@gmail.com>
2023-05-21 13:44:25 -0400

    Update Jeu.py

--- Jeu.py
+++ Jeu.py
@@ -17,32 +17,45 @@ def affichage (mot,lettre_utilisateur,mot_affiche):
     return mot_affiche
 
 run = True
-while run:# refaire une partie ?
+# refaire une partie ?
+score = 0
+
+while run:  #passser a un aurtre mot
     nombre_vies = 6
     mot = mot_aleatoire()
     mot_affiche = ['_ '] * len(mot)
     lettre_utilisateur = ''
 
-    while nombre_vies > 0: # Boucle du jeu
+
+    while nombre_vies > 0 and mot_affiche.count('_ ') != 0: # Boucle du jeu
+
         print('veuillez entrer une lettre :')
         print(f'il vous reste {nombre_vies} chances')
+        print(f'Score : {score}')
         affichage(mot,lettre_utilisateur,mot_affiche)
         print(mot_affiche)
         print(mot)
         if len(lettre_utilisateur) != 1:
             lettre_utilisateur=input()
+            print('Une lettre a la fois')
             continue
         if lettre_utilisateur in mot:
-            print('Bravo ! vous avez trouvé une lettre !')
+            print('\n \n \n \n \n \n \n \nBravo ! vous avez trouvé une lettre !')
             lettre_utilisateur = ''
 
         else:
             nombre_vies = nombre_vies - 1
-            print('ce n\'est pas la bonne lettre')
+            print('\n \n \n \n \n \n \n \nce n\'est pas la bonne lettre')
+            lettre_utilisateur = ''
+    if mot_affiche.count('_ ') == 0:
+        score = score + 1
+        print(f'Vous avez trouvé le mot !\n {mot}')
 
-
-    refaire = input('voulez vous refaire une partie [y]/[n] ?\n')
-    if refaire == 'y':
-        run = True
     else:
-        run = False
+        print(f'Vous avez perdu :\') \n Le mot etait {mot}')
+
+refaire = input('Voulez vous refaire une partie [y]/[n] ?\n')
+if refaire == 'y':
+    run = True
+elif refaire == 'n':
+    run = False
